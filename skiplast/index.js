cf3a7eb75796b37b2ff96728dcbc861bbff4004c
ahmed GHOUL <ghoul.ahmed5@gmail.com>
2019-02-27 22:05:20 +0100

    skipLast

---
+++ skiplast/index.js
@@ -0,0 +1,11 @@
+const { Observable, of } = require('rxjs');
+const { skipLast } = require('rxjs/operators');
+
+ // success 3
+of(1,2,3).pipe(
+    skipLast(2) 
+).subscribe(
+    success => console.log("success " + success),
+    err => console.log("err " + err),
+    () => console.log("complete"),
+)
